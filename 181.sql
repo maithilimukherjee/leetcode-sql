586930c975f8ca54b98d4361d5bd69075c492b7b
maithili <86041684+maithilimukherjee@users.noreply.github.com>
2026-08-09 12:15:16 +0530

    Create SQL query for employee salary comparison
    Added SQL query to find employees earning more than their managers.

---
+++ 181.sql
@@ -0,0 +1,23 @@
+Table: Employee
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| id          | int     |
+| name        | varchar |
+| salary      | int     |
+| managerId   | int     |
++-------------+---------+
+id is the primary key (column with unique values) for this table.
+Each row of this table indicates the ID of an employee, their name, salary, and the ID of their manager.
+ 
+
+Write a solution to find the employees who earn more than their managers.
+
+Return the result table in any order.
+
+# Write your MySQL query statement below
+SELECT a.name as Employee from Employee a
+JOIN Employee b
+ON a.managerId = b.id
+AND a.salary>b.salary;
